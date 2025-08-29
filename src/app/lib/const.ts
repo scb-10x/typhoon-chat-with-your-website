9fd3cec386bfb1c1494ced3f44d35226e1b978f9
kunato <kunato36@gmail.com>
2025-08-29 15:24:53 +0700

    update model list

--- src/app/lib/const.ts
+++ src/app/lib/const.ts
@@ -1,4 +1,4 @@
-export type TyphoonModel = 'typhoon-v2-8b-instruct' | 'typhoon-v2.1-12b-instruct' | 'typhoon-v2-70b-instruct' | 'typhoon-v2-r1-70b-preview';
+export type TyphoonModel = 'typhoon-v2.1-12b-instruct';
 
 export interface ModelParameters {
   name: string;
@@ -18,28 +18,28 @@ export const MODEL_PARAMETERS: Record<TyphoonModel, ModelParameters> = {
     description: 'Versatile 12B parameter model for all task',
     descriptionTh: 'โมเดลอเนกประสงค์ขนาด 12B พารามิเตอร์สำหรับทุกงาน'
   },
-  "typhoon-v2-70b-instruct": {
-    maxTokens: 1000,
-    temperature: 0.7,
-    maxContentLength: 12000 * 2,
-    name: 'Typhoon2 70B',
-    description: 'Powerful 70B parameter model for complex task',
-    descriptionTh: 'โมเดลประสิทธิภาพสูงขนาด 70B พารามิเตอร์สำหรับงานซับซ้อน'
-  },
-  "typhoon-v2-r1-70b-preview": {
-    maxTokens: 1000,
-    temperature: 0.7,
-    maxContentLength: 12000 * 2,
-    name: 'Typhoon2 R1 70B',
-    description: '70B model with strong reasoning capabilities',
-    descriptionTh: 'โมเดลขนาด 70B ที่มีความสามารถในการให้เหตุผลที่แข็งแกร่ง'
-  },
-  "typhoon-v2-8b-instruct": {
-    maxTokens: 1000,
-    temperature: 0.7,
-    maxContentLength: 12000 * 2,
-    name: 'Typhoon2 8B',
-    description: 'Efficient 8B parameter model for faster responses',
-    descriptionTh: 'โมเดลประสิทธิภาพดีขนาด 8B พารามิเตอร์สำหรับการตอบสนองที่เร็วขึ้น'
-  },
+  // "typhoon-v2-70b-instruct": {
+  //   maxTokens: 1000,
+  //   temperature: 0.7,
+  //   maxContentLength: 12000 * 2,
+  //   name: 'Typhoon2 70B',
+  //   description: 'Powerful 70B parameter model for complex task',
+  //   descriptionTh: 'โมเดลประสิทธิภาพสูงขนาด 70B พารามิเตอร์สำหรับงานซับซ้อน'
+  // },
+  // "typhoon-v2-r1-70b-preview": {
+  //   maxTokens: 1000,
+  //   temperature: 0.7,
+  //   maxContentLength: 12000 * 2,
+  //   name: 'Typhoon2 R1 70B',
+  //   description: '70B model with strong reasoning capabilities',
+  //   descriptionTh: 'โมเดลขนาด 70B ที่มีความสามารถในการให้เหตุผลที่แข็งแกร่ง'
+  // },
+  // "typhoon-v2-8b-instruct": {
+  //   maxTokens: 1000,
+  //   temperature: 0.7,
+  //   maxContentLength: 12000 * 2,
+  //   name: 'Typhoon2 8B',
+  //   description: 'Efficient 8B parameter model for faster responses',
+  //   descriptionTh: 'โมเดลประสิทธิภาพดีขนาด 8B พารามิเตอร์สำหรับการตอบสนองที่เร็วขึ้น'
+  // },
 }; 
